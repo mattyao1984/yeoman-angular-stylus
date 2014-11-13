5d33b379560694a4b0fb8f85598425b7e61afe19
Matt Yao <matt@arkade.com.au>
2014-11-13 17:36:31 +1100

    Project init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomanAngularStylusApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yomanAngularStylusApp
+ */
+angular.module('yomanAngularStylusApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomanAngularStylusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yomanAngularStylusApp
+ */
+angular.module('yomanAngularStylusApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
